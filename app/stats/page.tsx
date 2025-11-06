e982d7a71d717cb2a6ccf3138d23a4d2d862807c
Noya Halberstein <noyahalberstein@gmail.com>
2025-11-06 10:44:48 +0200

    add navigation

---
+++ app/stats/page.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material"
+
+const Page = ()=> {
+    return <Box>
+        stats
+    </Box>
+}
+
+export default Page
---
+++ components/Navigation.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { redirect,usePathname } from "next/navigation";
+
+export const Navigation = () => {
+  const pathname = usePathname()
+
+
+  return (
+    <ToggleButtonGroup
+      value={pathname}
+      exclusive
+      onChange={(_, value) => redirect(value)}
+      aria-label="tabs"
+      sx={{paddingY:'30px'}}
+    >
+      <Tab label="Reconcilation" value="/reconcile" />
+      <Tab label="Stats" value="/stats" />
+      <Tab label="Upload" value="/upload" />
+    </ToggleButtonGroup>
+  );
+};
+
+const Tab = ({ label, value }: { label: string; value: string }) => {
+  return (
+    <ToggleButton
+      value={value}
+      aria-label={value}
+      sx={{ textTransform: "none", minWidth:'150px' }}
+    >
+      {label}
+    </ToggleButton>
+  );
+};
